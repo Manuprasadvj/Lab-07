578863c0a008ff3dad0023c88a56c2d4ad4d5ee4
Manuprasadvj <Manuprasad.Variyathjayakumar@flemingcollege.ca>
2023-07-13 18:36:49 -0400

    edited

---
+++ create_db.py
@@ -0,0 +1,96 @@
+"""
+Description:
+ Creates the people table in the Social Network database
+ and populates it with 200 fake people.
+
+Usage:
+ python create_db.py
+"""
+import os
+import sqlite3
+import inspect
+from datetime import datetime
+from faker import Faker
+import random
+
+fake = Faker()
+
+# Determine the path of the database
+script_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(script_dir, 'social_network.db')
+
+def main():
+    create_people_table()
+    populate_people_table()
+
+def create_people_table():
+    """Creates the people table in the database"""
+    # TODO: Create function body
+    con = sqlite3.connect('social_network.db')
+
+    cur = con.cursor()
+
+    create_ppl_tbl_query = """
+        CREATE TABLE IF NON EXISTS people
+           (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            address TEXT NOT NULL,
+            city TEXT NOT NULL,
+            province TEXT NOT NULL,
+            bio TEXT,
+            age INTEGER,
+            created_at DATETIME NOT NULL,
+            updated_at DATETIME NOT NULL
+           );
+               """
+    
+
+    cur.execute(create_ppl_tbl_query)
+    con.commit()
+    con.close()
+
+    
+    return
+
+def populate_people_table():
+    """Populates the people table with 200 fake people"""
+    # TODO: Create function body
+    con = sqlite3.connect('social_network.db')
+    cur = con.cursor()
+
+    add_person_query = """ INSERT INTO people
+        (
+           name,
+           email,
+           address,
+           city,
+           province,
+           bio,
+           age,
+           created_at,
+           updated_at,
+
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+        """
+    for i in range(200):
+        new_person = (fake.name(),
+                    fake.ascii_company_email(),
+                    fake.street_address(),
+                    fake.city(),
+                    fake.administrative_unit(),
+                    fake.text(),
+                    random.randint(1, 100),
+                    datetime.now(),
+                    datetime.now())
+        cur.execute(add_person_query, new_person)
+        con.commit()
+        con.close()
+                    
+    
+    return
+
+if __name__ == '__main__':
+   main()
